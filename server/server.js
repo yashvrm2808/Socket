5cef0933ceb14701948e572c2bb9da908fd98537
Your Name <you@example.com>
2018-04-05 16:54:03 +0530

    connect disconnect event handler

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const express=require('express');
 const path=require('path');
+const http=require('http');//now we will create a server
+const socketIO=require('socket.io');
 const publicPath=path.join(__dirname ,'../public');
 const app= express();
 
 app.use(express.static(publicPath));
+var server=http.createServer(app);//server
+var io=socketIO(server);
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+
+socket.on('disconnect',()=>{
+    console.log('Disconnected');
+})    
+});
 
 
 
 var port=process.env.PORT||3000;
-app.listen(port);
+server.listen(port);
 console.log(`Server is running ${port}`);
