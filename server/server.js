ab410d60e6d34c701477a5892f61eea8adb0e618
Your Name <you@example.com>
2018-04-05 18:09:13 +0530

    newmessage CreateMessage event handler

--- server/server.js
+++ server/server.js
@@ -11,8 +11,26 @@ var io=socketIO(server);
 io.on('connection',(socket)=>{
     console.log('New user connected');
 
+    socket.emit('newEmail',{
+        from:'mike@gmail.com',
+        text:'Whats going on',
+        createAt: '5:28 pm'
+    });
+    socket.emit('newMessage',{
+        from:'yash@gmail.com',
+        text:'Hey Vivek',
+        createAt:'default'
+    });
+    socket.on('createMessage',(createMessage)=>{
+        console.log('createMessage',createMessage)
+    })
+    socket.on('createEmail',(newEmail)=>{
+        console.log('createEmail',newEmail)
+    })
+
 socket.on('disconnect',()=>{
     console.log('Disconnected');
+
 })    
 });
 
