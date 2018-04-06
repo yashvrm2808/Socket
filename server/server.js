088f0a80965259849560f7111c3638178c42f80d
Your Name <you@example.com>
2018-04-06 12:47:33 +0530

    multi connection

--- server/server.js
+++ server/server.js
@@ -1,32 +1,28 @@
-const express=require('express');
-const path=require('path');
-const http=require('http');//now we will create a server
-const socketIO=require('socket.io');
-const publicPath=path.join(__dirname ,'../public');
-const app= express();
-
+const express = require('express');
+const path = require('path');
+const http = require('http'); //now we will create a server
+const socketIO = require('socket.io');
+const publicPath = path.join(__dirname, '../public');
+const app = express();
 app.use(express.static(publicPath));
-var server=http.createServer(app);//server
-var io=socketIO(server);
-io.on('connection',(socket)=>{
+var server = http.createServer(app); //server
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
     console.log('New user connected');
+    socket.on('createMessage', (createMessage) => {
+        console.log('createMessage', createMessage)
+        io.emit('newMessage', {
+            from: createMessage.from,
+            text: createMessage.text,
+            createdAt: new Date().getDate()
+        });
+    });
+    socket.on('disconnect', () => {
+        console.log('Disconnected');
+
+    })
 })
-    socket.on('createMessage',(createMessage)=>{
-        console.log('createMessage',createMessage)
-    io.emit('newMessage',{
-        from:createMessage.from,
-        text:createMessage.text,
-        createdAt:new Date().getDate()
-    })
-    })
-    socket.on('disconnect',()=>{
-    console.log('Disconnected');
-
-})    
-
-
-
-
-var port=process.env.PORT||3000;
+var port = process.env.PORT || 3000;
 server.listen(port);
 console.log(`Server is running ${port}`);
