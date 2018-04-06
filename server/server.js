076a4b8678eef31fa8a93851ffbd30c203856ebf
Your Name <you@example.com>
2018-04-06 16:26:14 +0530

    multi connection

--- server/server.js
+++ server/server.js
@@ -10,7 +10,18 @@ var io = socketIO(server);
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-socket.on('createMessage', (createMessage) => {
+   socket.emit('newMessage',{
+       from:"Admin",
+       text:"welcome to the chat",
+       createdAt:new Date().getTime()
+   })
+   socket.broadcast.emit('newMessage',{
+       from:'Admin',
+       text:'New user Connected',
+       createdAt:new Date().getTime()
+   });
+   
+    socket.on('createMessage', (createMessage) => {
         console.log('createMessage', createMessage)
         io.emit('newMessage', {
             from: createMessage.from,
@@ -20,9 +31,9 @@ socket.on('createMessage', (createMessage) => {
     });
     socket.on('disconnect', () => {
         console.log('Disconnected');
-});
-});
 
+    })
+})
 var port = process.env.PORT || 3000;
 server.listen(port);
 console.log(`Server is running ${port}`);
